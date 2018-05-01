7821343a440fcd87540eb5dc3b48031c768d6e79
Jason Liu <jasobs10@gmail.com>
2018-05-01 16:31:56 -0400

    Add spice and seasoning to splash page

--- src/components/auth/splash.jsx
+++ src/components/auth/splash.jsx
@@ -40,51 +40,83 @@ const Splash = ({ showModal, hideModal }) => {
     <div className="splash-wrapper">
       <section className="splash-header">
         <div className="login-bar">
-          <div>
-            <span>The Book</span>
-          </div>
           <div className="login-buttons-container">
             <button onClick={() => showModal(buildSessionType('login'))}>Log In</button>
             <button onClick={() => showModal(buildSessionType('signup'))}>Sign Up</button>
           </div>
         </div>
-        <div className="splash-header-body">
-          body
-        </div>
-      </section>
-      <section className="splash-body">
-        <h2>pick against your friends family and co-workers</h2>
-        <div className="splash-body-main">
-          <div className="splash-features-container">
-            <div>
-              <img src={createLeague} alt="create"/>
+        <div className="splash-body">
+          <h2>welcome to the book</h2>
+          <h3>the antithesis of the casual office league</h3>
+          <div className="splash-body-main">
+            <div className="splash-features-container">
               <div>
-                create private leagues and invite your friends
+                <img src={createLeague} alt="create"/>
+                <div>
+                  create private leagues and invite your friends
+                </div>
               </div>
-            </div>
-            <div>
-              <img src={winMoney} alt="money"/>
               <div>
-                set buy in amount and payout rules
+                <img src={winMoney} alt="money"/>
+                <div>
+                  set buy in amount and payout rules
+                </div>
               </div>
-            </div>
 
-            <div>
-              <img src={inviteFriends} alt="invite"/>
               <div>
-                join or request access to your friends' private leagues
+                <img src={inviteFriends} alt="invite"/>
+                <div>
+                  join or request access to your friends' private leagues
+                </div>
               </div>
-            </div>
 
-            <div>
-              <img src={podium} alt="podium"/>
               <div>
-                Set custom rankings and scores for your league
+                <img src={podium} alt="podium"/>
+                <div>
+                  Set custom rankings and scores for your league
+                </div>
               </div>
             </div>
           </div>
+          <button className="session-button session-button-large" onClick={() => showModal(buildSessionType('signup'))}>SIGN UP TODAY</button>
         </div>
       </section>
+      <section className="splash-center">
+        <h1>PLAY WATCH AND WIN</h1>
+      </section>
+      <section className="splash-center">
+        <div>
+        <h2>About</h2>
+          <p>
+            Welcome to the most competitive, sophisticated, and intensive NFL ATS pick'em league you will ever join.
+            The antithesis of the casual office league, this is a group of elite handicappers who take pride, each and every week of the
+            season, in their ability to distinguish winners from losers. For those of you who are into making picks by throwing darts an
+            hour before the games kickoff, you would do well to look elsewhere. Put a little more bluntly: if you are the type of person
+            who suffers from mild forms of depression after a bad week (and yes, they will happen),
+            then this league is probably for you - otherwise, you're just throwing your money away to the sharks
+            (which, of course, they will gladly take).
+          </p>
+          <p>
+            All jokes aside, if you enjoy picking NFL games against the spread,
+            you should strongly consider joining this pick'em league.
+            It makes literally every game interesting - including when the Browns play
+            the Raiders in a Week 17 game more worthless than Enron stock, your eyes will
+            remain glued to the TV while Oakland drives down the field to attempt an otherwise
+            meaningless back-door cover. It's also a great subject for smack talk, of which there will be plenty.
+            It's a lot of fun and, arguably, takes more skill than most fantasy sports games,
+            including the ever popular fantasy football. Still need convincing? Talk to the person who
+            referred you to the site - they should be able to tell you everything you need to know.
+          </p>
+        </div>
+
+        <div>
+          <h2>Our Sponsors</h2>
+          test
+        </div>
+
+
+      </section>
+
     </div>
   );
 }
